090ed516a9688ea6334afecdce5a40da077ac4c9
IvanDario <IvanDario@Ivan-PC>
2016-11-05 22:36:40 -0500

    holi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import processing.core.PApplet;
 
-public class Main {
+public class Main extends PApplet {
+
+	static public PApplet app;
+	Logica log;
+
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+
+	@Override
+	public void settings() {
+		size(1200, 700);
+	}
+
+	@Override
+	public void setup() {
+		Main.app = this;
+		log = new Logica();
+
+	}
+
+	@Override
+	public void draw() {
+		background(0);
+
+	}
+
 
 }
