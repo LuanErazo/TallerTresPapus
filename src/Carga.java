b26ef46ddba603e746a4d4b72090d385f841e8a0
IvanDario <IvanDario@172.30.184.34>
2016-11-11 17:59:15 -0500

    carga de imagenes y el data de los PImages

--- src/Carga.java
+++ src/Carga.java
@@ -1,28 +1,31 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PShape;
 
 public class Carga {
 	private PApplet app;
-	public static PShape carro;
-	public static PShape player;
-	public static PShape enemigo;
-	public static PShape bomba;
-	public static PShape sparkie;
-	public static PShape sparkieReal;
-	public static PShape fondoCarro;
-	public static PShape fondoEnemigo;
-	public static PShape instrucciones;
+	public static PImage carro;
+	public static PImage player;
+	public static PImage enemigo;
+	public static PImage bomba;
+	public static PImage sparkie;
+	public static PImage sparkieReal;
+	public static PImage fondoCarro;
+	public static PImage fondoEnemigo;
+	public static PImage instrucciones;
+	public static PImage inicio;
 
 	public Carga() {
-		carro = app.loadShape("../data/");
-		player = app.loadShape("../data/");
-		enemigo = app.loadShape("../data/");
-		bomba = app.loadShape("../data/");
-		sparkie = app.loadShape("../data/");
-		sparkieReal = app.loadShape("../data/");
-		fondoCarro = app.loadShape("../data/");
-		fondoEnemigo = app.loadShape("../data/");
-		instrucciones = app.loadShape("../data/");
+		carro = app.loadImage("../data/imagenMJ2.png");
+		player = app.loadImage("../data/carrito.png");
+		enemigo = app.loadImage("../data/enemigo.png");
+		bomba = app.loadImage("../data/bomba.png");
+		sparkie = app.loadImage("../data/sparkie.png");
+		sparkieReal = app.loadImage("../data/sparkieReal.png");
+		fondoCarro = app.loadImage("../data/pantallaTres.png");
+		fondoEnemigo = app.loadImage("../data/pantallaCuatro.png");
+		instrucciones = app.loadImage("../data/pantallaDos.png");
+		inicio = app.loadImage("../data/pantallaUno.png");
 
 	}
 
