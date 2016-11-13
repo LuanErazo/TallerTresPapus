951de2c6936cac16522fa21732d66a39e889237b
Julio César <Julio Csar@DESKTOP-UPH1PD4>
2016-11-13 12:44:28 -0500

    Carga las imagenes de fondo y están dentro de un swtich para el cambio.

--- src/Logica.java
+++ src/Logica.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 
 import com.leapmotion.leap.Leap;
@@ -14,61 +15,90 @@ public class Logica {
 	private Player jugador;
 	private Hand der;
 	private Hand izq;
-	private PVector v; 
-	
+	private PVector v;
+	private PImage inicio, instrucciones, fondoCarro, fondoEnemigo;
+	private int cambio = 0;
+
 	public Logica() {
 		app = Main.app;
 		datos = new Carga();
 		leap = new LeapMotion(app);
 		der = leap.getRightHand();
 		izq = leap.getLeftHand();
-		v = new PVector(100,100);
+		v = new PVector(100, 100);
 		jugador = new Player(v);
-		
+		inicio = Carga.inicio;
+		instrucciones = Carga.instrucciones;
+		fondoCarro = Carga.fondoCarro;
+		fondoEnemigo = Carga.fondoEnemigo;
 	}
-	
-	public static boolean validar(float XUno, float YUno, float XDos, float YDos, float dist){
-		if(PApplet.dist(XUno, YUno, XDos, YDos)<= dist){
+
+	public static boolean validar(float XUno, float YUno, float XDos, float YDos, float dist) {
+		if (PApplet.dist(XUno, YUno, XDos, YDos) <= dist) {
 			return true;
 		}
 		return false;
 	}
-	
-	public void draww(){
+
+	public void draww() {
+		switch (cambio) {
+		case 0:
+
+			app.image(inicio, 0, 0);
+
+			break;
+		case 1:
+
+			app.image(instrucciones, 0, 0);
+
+			break;
+		case 2:
+
+			app.image(fondoCarro, 0, 0);
+
+			break;
+		case 3:
+
+			app.image(fondoEnemigo, 0, 0);
+
+			break;
+		}
 		leapMotionPlz();
-//		jugador.mover(izq.getStabilizedPosition());
+		// jugador.mover(izq.getStabilizedPosition());
 		jugador.display();
-//		System.out.println(izq.getPalmPosition().x + " " + izq.getPalmPosition().y);
+		// System.out.println(izq.getPalmPosition().x + " " +
+		// izq.getPalmPosition().y);
 		HUD();
+
 	}
-	
-	public void leapMotionPlz(){
-		 int fps = leap.getFrameRate();
+
+	public void leapMotionPlz() {
+		int fps = leap.getFrameRate();
 		for (Hand hand : leap.getHands()) {
-			PVector handStabilized     = hand.getStabilizedPosition();
+			PVector handStabilized = hand.getStabilizedPosition();
 			hand.draw();
 			app.pushMatrix();
-			app.translate(handStabilized.x, handStabilized.y,  handStabilized.z);
+			app.translate(handStabilized.x, handStabilized.y, handStabilized.z);
 			app.lights();
 			app.sphere(50);
 			app.popMatrix();
 		}
 	}
-	
-	public void MinimUso(){
-		
+
+	public void MinimUso() {
+
 	}
-	
-	private void clicked(){
-		
+
+	private void clicked() {
+
 	}
-	
-	private void mover(){
-		
+
+	private void mover() {
+
 	}
-	
-	private void HUD(){
-		
+
+	private void HUD() {
+
 	}
-	
+
 }
