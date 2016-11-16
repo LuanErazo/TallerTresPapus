86326e31de136cd0c3636b588815866836eb41a7
Julio César <Julio Csar@DESKTOP-UPH1PD4>
2016-11-15 23:50:48 -0500

    Tinte :)

--- src/Carro.java
+++ src/Carro.java
@@ -7,7 +7,7 @@ import sun.security.timestamp.TimestampToken;
 public class Carro {
 	private PApplet app;
 	int cellsize = 2; // Dimensions of each cell in the grid
-	int cols, rows;
+	int mira = 0;
 	private PImage carro;
 
 	public Carro() {
@@ -25,23 +25,41 @@ public class Carro {
 		PixelsDos();
 		
 	}
-
+		
 	public void pixels(){
 		/**
 		 * método pixel que me cambia el color de la imagen
 		 */
-		if (app.mouseX > app.width/2 && app.mouseX < app.width) {
+		if (app.mouseX > app.width/2 && app.mouseX < app.width && mira == 0) {
 			app.tint(255, 100, 50);
 			app.image(carro, app.width/2, app.height/2);
+		} else{
+			app.noTint();
 		}
 	}
 	
 	private void pixelsUno(){
-		
+		/**
+		 * método pixel que me cambia el color de la imagen
+		 */
+		if (app.mouseX > app.width/2 && app.mouseX < app.width && mira == 1) {
+			app.tint(100, 200, 50);
+			app.image(carro, app.width/2, app.height/2);
+		} else{
+			app.noTint();
+		}
 	}
 	
 	private void PixelsDos(){
-		
+		/**
+		 * método pixel que me cambia el color de la imagen
+		 */
+		if (app.mouseX > app.width/2 && app.mouseX < app.width && mira ==  2) {
+			app.tint(10, 20, 220);
+			app.image(carro, app.width/2, app.height/2);
+		} else{
+			app.noTint();
+		}
 	}
 	
 	
